0f0c0b6c095f039baeabc4f58862c1624c0ced41
diemoldau <davidzeng.42@gmail.com>
2016-02-05 22:16:36 -0500

    login

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,23 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, BooleanField, TextAreaField, FileField
+from wtforms.validators import DataRequired, Length
 from .models import User
+
+class LoginForm(Form):
+	nickname = StringField('nickname',validators= [DataRequired()])
+	password = StringField('password',validators =[DataRequired()])
+	remember_me = BooleanField('remember_me',default = False)
+
+class RegisterForm(Form):
+	nickname = StringField('openid',validators= [DataRequired()])
+	password = StringField('password',validators =[DataRequired()])
+	email = StringField('email',validators= [DataRequired()])
+
+	def validate(self):
+		if not Form.validate(self):
+			return False
+		user = User.query.filter_by(nickname = self.nickname.data).first()
+		if not user is None:
+			self.nickname.errors.append('Nickname taken. Choose another one')
+			return False
+		return True
--- app/models.py
+++ app/models.py
@@ -1,4 +1,35 @@
 from app import db
 from flask import url_for
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    password = db.Column(db.String(64), index = True)
+    email = db.Column(db.String(120), index=True)
+    about_me = db.Column(db.String(140))
+    profileName = db.Column(db.String(64))
 
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    def get_id(self):     
+        return unicode(self.id) 
+
+    @staticmethod
+    def make_unique_nickname(nickname):
+        version = 1
+        new_nickname = nickname
+        user = User.query.filter_by(nickname = new_nickname).first()
+        while user is not None:
+            version += 1
+            new_nickname = nickname + str(version)
+            user = User.query.filter_by(nickname = new_nickname).first()
+        return new_nickname
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
