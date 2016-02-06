9f2f37f8b8ddb9eff3945fce598dd34b22040e31
Bobbie Chen <bobbiechen75@gmail.com>
2016-02-06 00:10:37 +0000

    Rate form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField, TextAreaField, FileField
+from wtforms import StringField, BooleanField, TextAreaField, FileField, SelectField
 from wtforms.validators import DataRequired, Length
 from .models import User
 
@@ -24,4 +24,6 @@ class RegisterForm(Form):
 
 class ProfileForm(Form):
 	description = StringField('desc')
-	
+
+class RatingForm(Form):
+	rating = SelectField('rating', choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')])
